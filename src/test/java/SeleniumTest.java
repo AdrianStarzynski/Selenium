c2de8597a722a0089a118d7e3e5299e528d1afbb
AdrianStarzynski <adrian.starzynski.it@gmail.com>
2023-06-02 15:26:54 +0200

    chromeDriver

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,10 +1,16 @@
+import com.google.common.annotations.VisibleForTesting;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class SeleniumTest {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
     }
+    @Test
+    public void openGooglePage(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Adrian\\Documents\\GitHub\\Selenium\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://github.com/AdrianStarzynski");
+    }
 }
-
-    Miniaturka wykładu
