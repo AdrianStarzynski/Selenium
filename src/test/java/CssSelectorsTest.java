c9aa9f07e97c42fe6c9e399a6fbcde3b5b795c92
AdrianStarzynski <adrian.starzynski.it@gmail.com>
2023-06-05 23:41:44 +0200

    CSS Selectors

--- src/test/java/CssSelectorsTest.java
+++ src/test/java/CssSelectorsTest.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.text.BreakIterator;
+
 public class CssSelectorsTest {
     @Test
     public void findElements(){
@@ -27,8 +29,47 @@ public class CssSelectorsTest {
 
         By cssClass2 = By.cssSelector("[class='topSecret']");
         driver.findElement(cssClass2);
+        //5) Search all tags on webside - in console $$("*")
+        By all = By.cssSelector("*");
+        driver.findElement(all);
+        //6) Search ul inside Div, tr in table, tr in table body;
+        // in devtools console - $$("div ul")
+        // in devtools console - $$("table tr")
+        // in devtools console - $$("tbody tr")
 
+        By ulInsideDiv = By.cssSelector("div ul");
+        By trInTable = By.cssSelector("table tr");
+        By trBody = By.cssSelector("tbody tr");
 
+        driver.findElement(ulInsideDiv);
+        driver.findElement(trInTable);
+        driver.findElement(trBody);
+
+        //7) Seach only first elelment on (selector) - in console $$("div >tbody")
+        By firstChildUlInDiv = By.cssSelector("div > ul");
+        By firstChildTrInTbody = By.cssSelector("tbody > tr");
+
+        driver.findElement(firstChildTrInTbody);
+        driver.findElement(firstChildUlInDiv);
+
+        //8) Seach only first elelment after label - in console $$(label + from)
+        By firstFromAfterLabel = By.cssSelector("label + form");
+        driver.findElement(firstFromAfterLabel);
+        //8.1) Search all elemets after label - in console $$(label ~ from)
+        By aLlFromAfterLabel = By.cssSelector("label ~ form");
+        driver.findElement(aLlFromAfterLabel);
+
+        //9) Search only part of elements
+
+        By attrTag = By.cssSelector("input[name= 'fname']");
+        By attrContains = By.cssSelector("[name*='name']");
+        By attrStarts = By.cssSelector("[name^='f']");
+        By attrEnds = By.cssSelector("[name$='name']");
+
+        driver.findElement(attrTag);
+        driver.findElement(attrContains);
+        driver.findElement(attrStarts);
+        driver.findElement(attrEnds);
 
     }
 }
